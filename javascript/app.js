f84cb85c50b8cbe1152651633d4deeea3fe96336
Jimi-Bada <jimibadaadedamola@gmail.com>
2023-07-25 17:20:05 +0100

    Emma Jagaban

---
+++ javascript/app.js
@@ -0,0 +1,24 @@
+jarallax(document.querySelectorAll('.jarallax'), {
+    // options here
+});
+const slidingNav = document.querySelector('.sliding-navigation');
+const slidingNavButton = document.querySelector('.sliding-nav-button');
+const navButton = document.querySelector('.nav-button');
+const nav = document.querySelector('.navigation');
+const topBar = document.querySelector('.topbar');
+let buttonClicked = false;
+console.log(slidingNav);
+window.onscroll= function(){
+    scroll();
+}
+function scroll(){
+    if(document.body.scrollTop > 80 || document.documentElement.scrollTop > 80 ){
+        nav.classList.add('sliding-navigation');
+        topBar.style.display='none';
+        
+    }else{
+        nav.classList.remove('sliding-navigation');
+        topBar.style.display='block';
+    }
+}
+
